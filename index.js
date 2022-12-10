3c06df88ce16aad7133f8dabe25066277270531f
Srushti Basavaraddi <sbasavaraddi@gamil.com>
2022-12-11 01:41:48 +0530

    reply added

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 //Load HTTP module
 import express from "express";
+import bodyParser from "body-parser";
 
 const app =express();
 const port =9000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use("/",(req,res)=>{
-    res.json({message:"hello"})
+    res.json({message:"hellog"})
 });
 
 app.post("/",(req,res)=>{
