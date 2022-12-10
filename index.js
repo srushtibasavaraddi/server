3bb32eeb936271b4aa38f412f8e1c53cefb3ccb5
Srushti Basavaraddi <sbasavaraddi@gamil.com>
2022-12-11 02:37:13 +0530

    r3

--- index.js
+++ index.js
@@ -8,11 +8,19 @@ const port =9000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use("/",(req,res)=>{
-    res.json({
-        "reply":"ok"
-    })
-})
+var temp;
+app.post("/",(req,res)=>{
+        temp=req.body.message;
+        if(temp=="support"){
+            res.json({
+                "reply":"no"
+        })}
+        else{
+            res.json({
+                "reply":"ok "
+        })
+    }
+ })
 app.listen(port,()=>{
     console.log(`start`);
 })
